2fd8a4117cc0728b7374be8dbbfdbbac6c9b67bd
tosvt <sweb7@ya.ru>
2022-03-09 03:46:40 +0300

    Adding a Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,29 +4,62 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public ControlType controlType;
+    public Joystick joystick;
     public float speed; //скорость игрока
 
+    public enum ControlType{PC, Android}
+
     private Rigidbody2D rb; // его риджитбоди
-
     private Vector2 moveInput; // считываение в какую сторону мы движемся
-
     private Vector2 moveVelocity; //итоговая скорость игрока в каком-то направлении
+    private Animator anim; // указываем аниматор    
+
+    private bool facingRight = true; // true когда игрок смотрит вправо
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // получаем компонент Rigidbody2D
+        anim = GetComponent<Animator>(); //указываем аниматор в стартовой функции
+        if(controlType == ControlType.PC){//если входим в игру в режиме PC - джойстик отключается
+            joystick.gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); // здесь moveInput считывает горизонтальное и вертикальное значение
+        if(controlType == ControlType.PC){
+            moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); // здесь moveInput считывает горизонтальное и вертикальное значение 
+        }
+        else if(controlType == ControlType.Android){
+            moveInput = new Vector2(joystick.Horizontal, joystick.Vertical); // здесь moveInput считывает горизонтальное и вертикальное значение 
+        }
         moveVelocity = moveInput.normalized * speed;
+
+        if(moveInput.x == 0){ //если мы стоим на месте
+            anim.SetBool("isRunning", false); //проигрывается стандартаня анимация, когда мы стоим
+        }
+        else {
+            anim.SetBool("isRunning", true);//иначе анимация бега
+        }
+
+        if(!facingRight && moveInput.x > 0){ // если мы смотрим влево и идем вправо
+            Flip(); // то происходит разворот
+        } else if(facingRight && moveInput.x < 0){ // если мы смотрим вправо и идем влево
+            Flip(); // то происходит разворот
+        }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    private void Flip(){ // разворачивает игрока в другую сторону
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 }
