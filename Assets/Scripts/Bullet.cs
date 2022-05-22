726e30e244f03cd0f8b47ccd538d5f263a899965
tosvt <sweb7@ya.ru>
2022-05-23 01:47:05 +0300

    Updated Effects

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,7 +10,11 @@ public class Bullet : MonoBehaviour
    public int damage; // урон от пули
    public LayerMask whatIsSolid; // что пуля будет пробивать (что считать твердым)
    public GameObject AttackEffect;
+   public Animator camAnim;
 
+    private void Start() {
+        camAnim = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>(); //анимация камеры
+    }
     private void Update(){
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, whatIsSolid); // нахождение объекта для пробития
         if(hitInfo.collider != null){ //если пуля столкнулась с каким-то коллайдером
@@ -18,6 +22,7 @@ public class Bullet : MonoBehaviour
                 hitInfo.collider.GetComponent<Enemy>().TakeDamage(damage); // наносим урон врагу
             }
             Instantiate(AttackEffect, transform.position, Quaternion.identity);
+            camAnim.SetTrigger("shake");
             Destroy(gameObject); //уничтожаем патрон
         }
         transform.Translate(Vector2.up * speed * Time.deltaTime); //двжиение патрона
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,9 @@ public class Player : MonoBehaviour
     private Vector2 moveInput; // считываение в какую сторону мы движемся
     private Vector2 moveVelocity; //итоговая скорость игрока в каком-то направлении
     private Animator anim; // указываем аниматор    
-    public GameObject PlayerEffect;
+    public GameObject PlayerEffect; // эффект
+    public GameObject effect;
+    private Animator camAnim;
     private bool facingRight = true; // true когда игрок смотрит вправо
     // Start is called before the first frame update
     void Start()
@@ -22,7 +24,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); // здесь moveInput считывает горизонтальное и вертикальное значение 
         moveVelocity = moveInput.normalized * speed;
 
